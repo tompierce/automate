259e6da6b5533a28382d8eaf93deb91bb092b42d
Tom Pierce <tom.pierce0@gmail.com>
2016-01-19 19:10:23 +0000

    Fixed git trigger

--- server/triggers/GitTrigger.py
+++ server/triggers/GitTrigger.py
@@ -16,20 +16,25 @@ class GitTrigger(Trigger):
         temp_repo_dir = '/tmp/automate/git-trigger/' + self.job_id
         temp_repo_git = temp_repo_dir + '/.git'
 
-        git = sh.git.bake('-C \"' + temp_repo_dir + '\"')
+        git = sh.git
 
         if not os.path.isdir(temp_repo_dir):
             os.makedirs(temp_repo_dir)
 
         if not os.path.isdir(temp_repo_git):
             git.clone(self.trigger_data['repository'], temp_repo_dir)
+            logging.debug('Firing Git Trigger')
             return datetime.now()
         else:
+            git = git.bake('-C', temp_repo_dir)
             git.fetch('origin')
             status_str = git.status()
+            logging.debug(status_str)
             if 'branch is behind' in status_str:
                 git.merge('origin/master')
+                logging.debug('Firing Git Trigger')
                 return datetime.now()
 
+        logging.debug('Not firing Git Trigger')
         return const.DATETIME_NEVER
 
